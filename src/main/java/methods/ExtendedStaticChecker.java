9fd1fee7f83fac762105ce23f4b62cfde889a756
John L. Singleton <jsinglet@gmail.com>
2015-05-13 11:48:54 -0400

    Initial import

---
+++ src/main/java/methods/ExtendedStaticChecker.java
@@ -0,0 +1,10 @@
+package methods;
+
+import verily.lang.*;
+
+public class ExtendedStaticChecker{
+
+     public static final String check(String Source){
+          return "This is a checked program";
+     }
+}
---
+++ src/main/java/routers/ExtendedStaticChecker.java
@@ -0,0 +1,10 @@
+package routers;
+
+import verily.lang.*;
+
+public class ExtendedStaticChecker{
+
+     public static final Content check(String Source, String result){
+          return new TextContent(Source);
+     }
+}
