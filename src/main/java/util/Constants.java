aed108437a6c2caacbde9d7189a6a0241dd64166
John L. Singleton <jsinglet@gmail.com>
2015-05-26 13:43:34 -0400

    Made things more exciting!

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -3,7 +3,7 @@ package util;
 public class Constants {
     public static final String version = "1.0";
     public static final String VERILY_TAGLINE_MD = "\n\n----\n" +
-            "*This TryOpenJML API call is brought to you by the Verily Web Framework: [http://goverily.org](http://goverily.org)*\n";
+            "*This TryOpenJML API call is brought to you by the Verily Web Framework: [http://goverily.org](http://goverily.org)!!*\n";
     public static final String VERILY_TAGLINE_TXT = "\n\n----\n" +
-            "This TryOpenJML API call is brought to you by the Verily Web Framework: http://goverily.org\n";
+            "This TryOpenJML API call is brought to you by the Verily Web Framework: http://goverily.org!!\n";
 }
