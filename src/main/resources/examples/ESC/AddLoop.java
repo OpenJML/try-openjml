dd0cc1c14c1856ea90138b859a2f43ddf0f80422
John L. Singleton <jsinglet@gmail.com>
2016-05-20 12:20:11 -0400

    more examples

---
+++ src/main/resources/examples/ESC/AddLoop.java
@@ -0,0 +1,17 @@
+public class AddLoop{
+     //@ requires y >= 0;
+     //@ ensures \result == x + y;
+     public static int Add (int x, int y){
+         int sum = x;
+         int n = y;
+         //@ maintaining sum == x + y - n && 0 <= n;
+         //@ decreases n;
+         while (n > 0)
+         {
+           sum = sum + 1;
+           n = n - 1;
+         }
+         return sum;
+
+     }
+ }
