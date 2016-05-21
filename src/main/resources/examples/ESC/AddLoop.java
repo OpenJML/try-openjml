4cf854c4232853bb46d1ba9d9d485db61e28ef65
Yuyan Bao <baoyuyan@hotmail.com>
2016-05-21 18:54:26 -0400

    revised the AddLoop example; added the BinarySearch example;

--- src/main/resources/examples/ESC/AddLoop.java
+++ src/main/resources/examples/ESC/AddLoop.java
@@ -1,9 +1,11 @@
-public class AddLoop {
-     //@ requires y >= 0;
+public class Add{
+     //@ requires true;
      //@ ensures \result == x + y;
      public static int Add (int x, int y){
          int sum = x;
+         if (y > 0){
          int n = y;
+        
          //@ maintaining sum == x + y - n && 0 <= n;
          //@ decreases n;
          while (n > 0)
@@ -11,7 +13,20 @@ public class AddLoop {
            sum = sum + 1;
            n = n - 1;
          }
-         return sum;
+         }
+         else{
+           int n = -y;
+          
+         //@ maintaining sum == x + y + n && 0 <= n;
+         //@ decreases n;
+         while (n > 0)
+         {
+           sum = sum - 1;
 
+           n = n - 1;
+         }
+         }
+         return sum;
+       
      }
- }
+}
