a2d612834c36d6453fc952dba4badf1c34172ee0
John L. Singleton <jsinglet@gmail.com>
2016-06-07 15:02:19 -0400

    testing newlines

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String VERILY_TAGLINE_MD = "\n\n----\n" +
             "*This API call handled by the Verily Web Framework: [http://goverily.org](http://goverily.org)*\n";
     public static final String VERILY_TAGLINE_TXT = "\n\n----\n" +
-            "This API call handled by the Verily Web Framework: http://goverily.org\n";
+            "This API call handled by the Verily Web Framework: http://goverily.org\ntest\ntest";
 }
