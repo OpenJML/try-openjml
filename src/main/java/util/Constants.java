5e27f5d1311cd8fa32d10b6968ac19b1bde6d06a
John L. Singleton <jsinglet@gmail.com>
2016-06-07 15:03:55 -0400

    test

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final int MAX_JOBS = 4;
     public static final String version = "1.0";
     public static final String VERILY_TAGLINE_MD = "\n\n----\n" +
-            "*This API call handled by the Verily Web Framework: [http://goverily.org](http://goverily.org)*\n";
+            "*This API call handled by the Verily Web Framework: [http://goverily.org](http://goverily.org)*\ntest \n\n test";
     public static final String VERILY_TAGLINE_TXT = "\n\n----\n" +
-            "This API call handled by the Verily Web Framework: http://goverily.org\ntest\ntest";
+            "This API call handled by the Verily Web Framework: http://goverily.org\n";
 }
