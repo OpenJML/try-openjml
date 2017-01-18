905e87c4423705dc6fbd3af7ee38bfc22faa565f
John L. Singleton <jsinglet@gmail.com>
2017-01-18 15:30:52 -0500

    added MaybeAdd example

--- scripts/make.sh
+++ scripts/make.sh
@@ -8,4 +8,5 @@ node convert-examples.js \
 ../src/main/resources/examples/ESC/FindFirstZero.java  \
 ../src/main/resources/examples/ESC/Inverse.java  \
 ../src/main/resources/examples/ESC/Smallest.java  \
+../src/main/resources/examples/ESC/MaybeAdd.java  \
 ../src/main/resources/ExtendedStaticCheckerMetadata.json > ../src/main/resources/ExtendedStaticCheckerMetadata.ftl
